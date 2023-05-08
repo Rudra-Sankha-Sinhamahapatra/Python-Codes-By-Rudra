0ad2dac800f01db26300c4a64ffafcd10791642c
Rudra-Sankha-Sinhamahapatra <rssmp120@gmail.com>
2023-05-08 22:31:17 +0530

    update
    update

---
+++ var.py
@@ -0,0 +1,18 @@
+# Declaring and assigning value to a variable
+a="Apple"
+print(a)
+
+#changing value of variable
+a="Aeroplane"
+print(a)
+
+a=100
+print(a)
+
+#Assigning Multiple values to a variable
+b,c,d=1,2.5, "Hello"
+print(b,c,d)
+
+#Assigning same value to multiple variables
+b=c=d=5
+print(b,c,d)
